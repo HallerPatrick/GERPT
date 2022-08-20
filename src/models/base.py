9c70318da09f53fd4b5401984f56458cdc5d403e
Patrick Haller <patrickhaller40@googlemail.com>
2022-08-20 15:20:12 +0200

    Flops

--- src/models/base.py
+++ src/models/base.py
@@ -26,10 +26,13 @@ class BasePLModel(pl.LightningModule):
 
         if torch.cuda.is_available():
             if batch_idx == 0:
+                print(f"Total duration of forward pass: {self.flop_profiler.get_total_duration()}")
+                print(f"Total FLOPS: {self.flop_profiler.get_total_flops()}")
                 flops = self.flop_profiler.get_total_flops(as_string=True)
                 params = self.flop_profiler.get_total_params(as_string=True)
                 self.flop_profiler.print_model_profile(profile_step=batch_idx)
                 self.flop_profiler.end_profile()
+
 
         return super().on_train_batch_end(outputs, batch, batch_idx, unused)
 
