a72b03cb2646e7c76713a2a9177261162adfce07
Patrick Haller <patrickhaller40@googlemail.com>
2022-08-20 13:53:32 +0200

    preprocess script

---
+++ preprocess.py
@@ -0,0 +1,27 @@
+
+
+import torch
+from src.args import parse_args
+from src.dataset import load_tokenized_dataset
+
+
+def main():
+
+    args = parse_args()
+
+    tokenized_dataset, dictionary = load_tokenized_dataset(
+        args.bptt,
+        args.ngram,
+        args.max_dict_size,
+        args.unk_threshold,
+        args.fallback,
+        1,
+        *args.data.split("/"),
+        # cache_dir="/home/tmp/halerpat/datasets"
+    )
+
+    tokenized_dataset.save_to_disk(args.saved_data)
+    torch.save(dictionary, args.saved_dict)
+
+if __name__ == "__main__":
+    main()
